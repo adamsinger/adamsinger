f931436dcf80e0c7f0fcc761c5a74c1c0326d5b1
Adam Singer <adam@posterous-inc.com>
2012-02-20 14:41:00 -0800

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+About::Application.config.secret_token = '3cabc1d86084bb9661a697ddd77a11b0c06b34ac7e0d3251a8a88e7920c10936abf764d2726711502138b9ffebd700c412de91eaa8224aee20c88a89bab6b505'
